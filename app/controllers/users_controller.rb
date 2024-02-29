6206ba1b2199726a42b65acb49cf72573ef63e85
Cody Williams <cody@codywilliams.dev>
2024-02-28 19:42:05 -0800

    fix missing symbol colon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,27 +4,28 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json @users, status: :ok
+    render json: @users, status: :ok
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      render json @user, status: :created
+      render json: @user, status: :created
     else
-      render json @user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   def show
-    render json @user, status: :ok
+    render json: @user, status: :ok
   end
 
   def update
     if @user.update(user_params)
-      render json @user, status: :ok
+      render json: @user, status: :ok
     else
-      render json @user.errors, status: :unprocessable_entity
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
