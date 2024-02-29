7ca1ee17b4a61137ba9d2496ab5d1dcb8044393a
Cody Williams <cody@codywilliams.dev>
2024-02-28 19:11:59 -0800

    create jwt concern

---
+++ app/controllers/concerns/jwt.rb
@@ -0,0 +1,19 @@
+require 'jwt'
+
+module Jwt
+  extend ActiveSupport::Concern
+
+  protected
+
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def encode_token(payload, expiration = 7.days.from_now)
+    payload[:expiration] = expiration.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode_token(token)
+    decoded_token = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new(decoded)
+  end
+end
