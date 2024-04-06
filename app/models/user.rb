aefa4b5e1cf639ba58f3dabb95c3a326f24b47ee
Cody Williams <cody@codywilliams.dev>
2024-04-05 19:59:23 -0700

    require name length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :name, presence: true, length: { minimum: 1 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
