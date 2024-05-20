b13d2da95e7e0648e495e09e0f19d4a50f95c14a
Cody Williams <cody@codywilliams.dev>
2024-05-19 19:38:23 -0700

    include authenticator and error handler gems in api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,30 +1,8 @@
 module Api
   module V1
     class ApiController < ActionController::API
-      before_action :authenticate_user
-
-      private
-
-      def authenticate_user
-        authorization_header = User.find_by(auth_token: request.headers['Authorization'])
-        token = authorization_header.split(' ').last if authorization_header
-
-        if token
-          begin
-            decoded_token = decode_token(token)
-            user_id = decoded_token[:user_id]
-            @current_user = User.find(user_id)
-          rescue JWT::ExpiredSignature
-            render json: { error: 'Expired token' }, status: :unauthorized
-          rescue JWT::DecodeError
-            render json: { error: 'Invalid token' }, status: :unauthorized
-          rescue ActiveRecord::RecordNotFound
-            render json: { error: 'Login Error' }, status: :unauthorized
-          end
-        else
-          render json: { error: 'No token' }, status: :unauthorized
-        end
-      end
+      include ApiAuthenticator
+      include ApiErrorHandler
     end
   end
 end
