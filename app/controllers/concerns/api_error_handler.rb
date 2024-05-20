826b76f314e5cbd5314d2c6162e43507dc065579
Cody Williams <cody@codywilliams.dev>
2024-05-19 19:38:02 -0700

    make api error handler concern

---
+++ app/controllers/concerns/api_error_handler.rb
@@ -0,0 +1,15 @@
+module ApiErrorHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from StandardError, with: :render_error
+  end
+
+  private
+
+  def render_error(error)
+    raise error unless error.is_a?(StandardError)
+
+    render json: Auth::ApiErrorBlueprint.render(error), status: 200
+  end
+end
