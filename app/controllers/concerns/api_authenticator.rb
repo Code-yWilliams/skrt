68524619ccc0a452430c01d3417ce5c9f40d8b6b
Cody Williams <cody@codywilliams.dev>
2024-05-19 19:36:14 -0700

    move authentication to api authenticator concern

---
+++ app/controllers/concerns/api_authenticator.rb
@@ -0,0 +1,30 @@
+module ApiAuthenticator
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_user
+  end
+
+  private
+
+  def authenticate_user
+    authorization_header = request.headers['Authorization']
+    token = authorization_header.split(' ').last if authorization_header
+
+    if token
+      begin
+        decoded_token = decode_token(token)
+        user_id = decoded_token[:user_id]
+        @current_user = User.find(user_id)
+      rescue JWT::ExpiredSignature
+        raise ApiErrors::Auth::ExpiredTokenError
+      rescue JWT::DecodeError
+        raise ApiErrors::Auth::InvalidTokenError
+      rescue ActiveRecord::RecordNotFound
+        raise ApiErrors::Auth::InvalidTokenError
+      end
+    else
+      raise ApiErrors::Auth::UnauthorizedError
+    end
+  end
+end
