d5487c7abf90656a52e92be847ffa6ad6688a584
Cody Williams <cody@codywilliams.dev>
2024-06-05 18:51:07 -0700

    update user model column annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true, length: { minimum: 1 }
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :name, presence: true, length: { minimum: 1 }, on: :create
+  validates :email, presence: true, uniqueness: true, on: :create
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password_confirmation, presence: true, on: :create
 end
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id              :bigint           not null, primary key
-#  email           :string
-#  name            :string
-#  password_digest :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id                    :bigint           not null, primary key
+#  email                 :string
+#  name                  :string
+#  password_digest       :string
+#  refresh_token_version :integer          default(0), not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
 #
 # Indexes
 #
