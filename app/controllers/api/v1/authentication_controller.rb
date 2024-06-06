9ba0b4a60d25eb7646e53f16b1a858d0fc26e045
Cody Williams <cody@codywilliams.dev>
2024-06-05 18:52:48 -0700

    add token refresh api endpoint

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,16 +2,31 @@ module Api
   module V1
     class AuthenticationController < ApiController
       include Jwt
-      skip_before_action :authenticate_user, only: %i[login]
+      skip_before_action :authenticate_user, only: %i[login refresh]
 
       def login
         user = User.find_by(email: params[:email])
-        if user&.authenticate(params[:password])
-          token = encode_token(user_id: user.id)
 
-          render json: { token: token, user: { id: user.id, name: user.name, email: user.email } }, status: :ok
+        if user&.authenticate(params[:password])
+          access_token, refresh_token = AuthToken::GenerateService.call(user, increment_refresh_token: false)
+
+          render json: { access_token:, refresh_token:, user: { id: user.id, name: user.name, email: user.email } }, status: :ok
         else
-          render json: { error: 'unauthorized' }, status: :unauthorized
+          raise ApiErrors::Auth::UnauthorizedError
+        end
+      end
+
+      def refresh
+        refresh_token = params[:refresh_token]
+        decoded_refresh_token = decode_token(refresh_token)
+        user = User.find(decoded_refresh_token[:user_id])
+
+        if user.refresh_token_version == decoded_refresh_token[:version]
+          access_token, refresh_token = AuthToken::GenerateService.call(user)
+
+          render json: { access_token:, refresh_token:, user: { id: user.id, name: user.name, email: user.email } }, status: :ok
+        else
+          raise ApiErrors::Auth::UnauthorizedError
         end
       end
     end
