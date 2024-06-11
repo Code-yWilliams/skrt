1257b85fb508c93ff114eed2c806b77b5f4dff3b
Cody Williams <cody@codywilliams.dev>
2024-06-10 20:44:23 -0700

    make react app the root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
     filename = "#{route_parts.slice!(-1)}.rb"
     instance_eval(File.read(Rails.root.join(*(route_parts << filename))))
   end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   draw :api_routes
+
+  root "web_client#home"
 end
